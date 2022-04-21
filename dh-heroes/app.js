b11c72c38c829f91b8d925ae4cf165b96acad34e
pbidoggia <pbidoggia@digitalhouse.com>
2022-04-20 21:36:10 -0300

    some changes

--- dh-heroes/app.js
+++ dh-heroes/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+// views
+const views = path.join(__dirname, 'views/')
+// html
+const heroes = 'heroes.html'
+// http routes
+const httpHeroes = '/heroes'
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get(httpHeroes, (req, res) => {
+    res.sendFile(path.join(views, heroes))
+    //res.sendFile(__dirname + '/heroes.html')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+    console.log(__dirname)
 })
